340971cd2de5b06996ac29dfbdfcf3ff018825c6
Shawn Lin <>
2019-02-10 21:38:48 -0500

    Add database export

--- eod_download.py
+++ eod_download.py
@@ -2,17 +2,32 @@ import requests
 import yaml
 import pandas as pd
 import time
+import datetime
+
+from sqlalchemy import create_engine
 
 # Load configuration file
 with open("config.yml", "r") as cfgfile:
     cfg = yaml.load(cfgfile)
 
-# Assign initial api key
+# Configurations
+# -------------------------------------------------
+# API key for Alpha Vantage
 API_KEY = cfg["api_key"].pop()
-ticker_list = ["APRN", "JD", "TEAM", "CRM", "KO", "KHC", "LQD", "DIS", "FB", "GOOGL", "STZ"]
-
+# Database
+USER = cfg["mysql"]["user"]
+PASSWORD = cfg["mysql"]["password"]
+HOST = cfg["mysql"]["host"]
+PORT = cfg["mysql"]["port"]
+DATABASE = cfg["mysql"]["database"]
+DB_CON = create_engine("mysql+pymysql://{user}:{password}@{host}:{port}/{database}".format(
+    user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE))
 # Alpha Vantage URL
 API_URL = "https://www.alphavantage.co/query"
+
+# Tickers to download
+ticker_list = ["APRN", "JD", "TEAM", "CRM", "KO",
+               "KHC", "LQD", "DIS", "FB", "GOOGL", "STZ"]
 
 
 def download_price(ticker, api_key):
@@ -29,10 +44,11 @@ def download_price(ticker, api_key):
     data = r.json()
 
     # Convert json to dataframe
-    data = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient="index")
+    data = pd.DataFrame.from_dict(
+        data['Time Series (Daily)'], orient="index", dtype="float")
 
     # Assign date index to its own column
-    data["date"] = data.index
+    data["date"] = pd.to_datetime(data.index, format="%Y-%m-%d")
 
     # Add ticker to a new column
     data["ticker"] = ticker
@@ -47,7 +63,11 @@ def download_price(ticker, api_key):
                                 "4. close": "close", "5. adjusted close": "adj",
                                 "6. volume": "volume", "7. dividend amount": "dividend",
                                 "8. split coefficient": "split"})
-        
+
+    # Add completion timestamp
+    currentDT = datetime.datetime.now()
+    data["modified"] = currentDT
+
     return(data)
 
 
@@ -55,10 +75,16 @@ def download_price(ticker, api_key):
 for ticker in ticker_list:
     try:
         price_series = download_price(ticker=ticker, api_key=API_KEY)
-        print(price_series.tail())
+        price_series.to_sql(name="eod_stock_history",
+                            con=DB_CON, if_exists="append", index=False)
+        print(datetime.datetime.now(), "----", ticker,
+              "---- is written to price_data.eod_stock_history.")
     except:
         # Alpha Vantage limits the number of API calls per minute.
         time.sleep(60)
 
         price_series = download_price(ticker=ticker, api_key=API_KEY)
-        print(price_series.tail())
+        price_series.to_sql(name="eod_stock_history",
+                            con=DB_CON, if_exists="append", index=False)
+        print(datetime.datetime.now(), "----", ticker,
+              "---- is written to price_data.eod_stock_history.")

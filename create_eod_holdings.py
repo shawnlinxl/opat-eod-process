5ff0d2765e449be618c3e4eef5242268609b7231
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-02-26 22:13:48 -0500

    Moved from data-warehouse

---
+++ create_eod_holdings.py
@@ -0,0 +1,37 @@
+import datetime
+import pandas as pd
+import sqlalchemy as db
+import yaml
+import opat.portfolio
+
+# Load configuration file
+with open("config.yml", "r") as cfgfile:
+    cfg = yaml.load(cfgfile, Loader=yaml.FullLoader)
+
+# Configurations
+# -------------------------------------------------
+
+# Database
+USER = cfg["mysql"]["user"]
+PASSWORD = cfg["mysql"]["password"]
+HOST = cfg["mysql"]["host"]
+PORT = cfg["mysql"]["port"]
+DATABASE = "DW"
+DB_CON = db.create_engine("mysql+pymysql://{user}:{password}@{host}:{port}/{database}".format(
+    user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE)).connect()
+
+# Load transactions
+# -------------------------------------------------
+trades = pd.read_sql_query(
+    sql="SELECT tradeday, account, ticker, action, quantity FROM DW.trades", con=DB_CON)
+
+# Create holdings
+# -------------------------------------------------
+holdings = trades.groupby("account").apply(opat.portfolio.create_holdings)
+holdings = holdings.reset_index()
+holdings = holdings[["tradeday", "account", "ticker", "quantity"]]
+
+
+# Write holdings to database
+# -------------------------------------------------
+holdings.to_sql(name="eod_holdings", con=DB_CON, if_exists="replace", index=False)

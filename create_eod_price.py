1a09da94940ff5c49ad597b27101728850327491
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-09-17 23:48:43 -0400

    Sort before write to db.

--- create_eod_price.py
+++ create_eod_price.py
@@ -31,7 +31,6 @@ SELECT main.tradeday, main.ticker, open, high, low, close, adj, volume, dividend
        FROM DW.eod_price_archive
        GROUP BY tradeday, ticker) AS aux
     ON main.tradeday = aux.tradeday AND main.ticker = aux.ticker AND main.modified = aux.modified
- ORDER BY tradeday, ticker
 """
 
 price = pd.read_sql_query(sql=query, con=DB_CON)
@@ -44,7 +43,6 @@ query = """
 SELECT tradeday, ticker, open, high, low, close, adj, volume, dividend, split
   FROM DW.eod_price_archive 
  WHERE ticker = 'APRN' AND modified BETWEEN '2019-08-05' AND '2019-08-06'
- ORDER BY tradeday, ticker
 """
 
 aprn_fix = pd.read_sql_query(sql=query, con=DB_CON)
@@ -54,4 +52,4 @@ price = pd.concat([price, aprn_fix], ignore_index=True)
 
 # Write price data to database
 # -------------------------------------------------
-price.to_sql(name="eod_price", con=DB_CON, if_exists="replace", index=False)
+price.sort_values(by = ['tradeday', 'ticker']).reset_index(drop=True).to_sql(name="eod_price", con=DB_CON, if_exists="replace", index=False)

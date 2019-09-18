9b73b521a30930cc9fad0b32b2c67b545490ba24
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-09-17 23:33:52 -0400

    Add manual fix for APRN data source issue.

--- create_eod_price.py
+++ create_eod_price.py
@@ -36,6 +36,22 @@ SELECT main.tradeday, main.ticker, open, high, low, close, adj, volume, dividend
 
 price = pd.read_sql_query(sql=query, con=DB_CON)
 
+# Price for APRN is wrong historically after Aug
+# 05, 2019
+# -------------------------------------------------
+
+query = """
+SELECT tradeday, ticker, open, high, low, close, adj, volume, dividend, split
+  FROM DW.eod_price_archive 
+ WHERE ticker = 'APRN' AND modified BETWEEN '2019-08-05' AND '2019-08-06'
+ ORDER BY tradeday, ticker
+"""
+
+aprn_fix = pd.read_sql_query(sql=query, con=DB_CON)
+
+price = price[(price.ticker == 'APRN' & price.tradeday > "2019-08-05") | (price.ticker != 'APRN')]
+price = pd.concat([price, aprn_fix], ignore_index=True)
+
 # Write price data to database
 # -------------------------------------------------
 price.to_sql(name="eod_price", con=DB_CON, if_exists="replace", index=False)
